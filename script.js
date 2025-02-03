d1de8ddd5be60affedaf33a9c6618dc1a1d95303
leoninadev <149819978+leoninadev@users.noreply.github.com>
2025-02-03 18:48:43 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdowns = document.querySelectorAll('.dropdown');
+
+    dropdowns.forEach(dropdown => {
+        const dropbtn = dropdown.querySelector('.dropbtn');
+        const dropdownContent = dropdown.querySelector('.dropdown-content');
+
+        dropbtn.addEventListener('click', function(e) {
+            e.preventDefault(); // Impede o comportamento padrão do link
+             dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+        });
+        document.addEventListener('click', function(event) {
+                if (!dropdown.contains(event.target)) {
+                     dropdownContent.style.display = 'none';
+                }
+            });
+    });
+});
